f7c1fb0cfe1665fadc529a1008af3c00a264dcdc
Tmlost <arthur.castro@ort.org.br>
2024-10-05 13:00:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,15 +6,6 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
 }).addTo(map);
 
-// Add markers with popups
-const locations = [
-    { coords: [51.505, -0.09], name: "London" },
-    { coords: [40.7128, -74.0060], name: "New York" },
-    { coords: [35.6762, 139.6503], name: "Tokyo" },
-    { coords: [48.8566, 2.3522], name: "Paris" },
-    { coords: [-33.4489, -70.6693], name: "Santiago" },
-];
-
 locations.forEach(location => {
     L.marker(location.coords)
         .addTo(map)
